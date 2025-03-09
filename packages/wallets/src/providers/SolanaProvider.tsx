c21709d4ea476c4d37523e9b3cfe446b61f451ab
bitsbyritik <bitsbyritik@gmail.com>
2025-03-09 22:58:40 +0530

    Added solnana providers

---
+++ packages/wallets/src/providers/SolanaProvider.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React, { FC, ReactNode, useMemo } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import "@solana/wallet-adapter-react-ui/styles.css";
+
+interface Props {
+  children: ReactNode;
+}
+
+const SolanaProvider: FC<Props> = ({ children }) => {
+  const endpoint = useMemo(() => process.env.NETWORK_URL!, []);
+
+  const wallets = useMemo(() => [], [endpoint]);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default SolanaProvider;
